81e6caf56a905d8dd3856148001f4bdd7d38a10e
Max Ackerman <max@violetoffice.com>
2020-07-30 17:45:30 -0400

    screenshots!

---
+++ README.md
@@ -0,0 +1,7 @@
+Use [Puppeteer](https://pptr.dev/) to multiple device size screenshots from a list of URLs.
+
+Install: `npm install`
+Run: `npm run screenshot`
+
+- Add each URL as a new line in urls.txt
+- Browser sizes can be edited in `screenshot.js`
---
+++ screenshot.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer-core');
+const fs = require('fs');
+
+const urls = fs.readFileSync('urls.txt').toString().split("\n");
+
+const sizes = [
+  { name: 'iphone',
+    width: 375,
+    height: 667
+  },
+  {
+    name: 'desktop',
+    width: 1440,
+    height: 900
+  }
+]
+
+async function getUrlSizes(devices, urls) {
+  for (let url of urls) {
+    for (let size of sizes) {
+      console.log(`${size.name}: ${url}`)
+      try{
+        await getScreenShot(url, size);
+      }catch(e){
+        await console.error(`Error with ${url}`, e);
+      }
+    }
+  }
+}
+getUrlSizes(sizes, urls);
+
+async function getScreenShot(url, size){
+  const nodeurl = require("url");
+  const browser = await puppeteer.launch({
+    executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
+    // headless: false
+  });
+  const page = await browser.newPage();
+    await page.setViewport({
+    width: size.width,
+    height: size.height,
+    deviceScaleFactor: 2,
+  });
+  await page.goto(url, {waitUntil: 'networkidle2'}); // can also try networkidle0
+  // await page.waitFor(1000);
+  const parsed = nodeurl.parse(page._target._targetInfo.url);
+  const fileName = `${parsed.host}-${page._viewport.width}`;
+  await page.screenshot({path: `export/${fileName}.png`});
+  await browser.close();
+}
